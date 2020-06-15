532c25f196392b6a5f8f0fd2be1e9c12f692280e
Kyle Harrity <hpkyh47@gmail.com>
2020-06-14 22:45:28 -0400

    support for multiple file types

--- firefox/background.js
+++ firefox/background.js
@@ -15,8 +15,10 @@ function osNewLineType(os){
 		case "win":
 			return "\r\n";
 		default:
-			//uhhh, hope they're POSIX compliant
+			//uhhh, hope they're UNIX-like
 			return "\n";
+		//I also hope no one is browsing on an old old Mac
+		//or RISC-OS
 	}
 }
 
@@ -24,16 +26,75 @@ browser.contextMenus.create({	//contextMenus <- for chrome
 	id : "download-link-list",
 	title: "Make wget Download list",
 	contexts: ["all"]
-}, function(){console.log("success creating!");});
+});//, function(){console.log("success creating!");});
+
+browser.contextMenus.create({
+	id: "all",
+	title: "Get All Links",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "domain",
+	title: "Get All Links on this Domain",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "images",
+	title: "Get All Images",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "videos",
+	title: "Get All Videos",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "audio",
+	title: "Get All Audio",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "documents",
+	title: "Get All Documents",
+	contexts: ["all"]
+});
+
+browser.contextMenus.create({
+	id: "custom",
+	title: "Custom",
+	contexts: ["all"]
+});
 
 browser.contextMenus.onClicked.addListener(function(info, tab){
+	let port = browser.tabs.connect(tab.id);
 	switch(info.menuItemId){
 		case "download-link-list":
+		case "domain":
 			console.log("selected!: "+tab.id);
-			let id = tab.id;
-			//ask tab for list of links
-			let port = browser.tabs.connect(tab.id);
-			handleArchiveRequest(port);
+			handleArchiveRequest("link-list", port);
+			break;
+		case "images":
+			handleArchiveRequest("images", port);
+			break;
+		case "videos":
+			handleArchiveRequest("videos", port);
+			break;
+		case "audio":
+			handleArchiveRequest("audio", port);
+			break;
+		case "documents":
+			handleArchiveRequest("documents", port);
+			break;
+		case "custom":
+			handleArchiveRequest("custom", port);
+			break;
+		default:
+			console.log("ERROR: Unknown/unimplemented context menu option selected!");
 			break;
 	}
 });
@@ -42,11 +103,11 @@ browser.runtime.onMessage.addListener(function(msg){
 	console.log(msg);
 });
 
-function handleArchiveRequest(port){
+function handleArchiveRequest(action, port){
 	
 
 	port.postMessage({
-		"action":"link-list",
+		"action": action,
 		"newline": osNewLineType(platformInfo.PlatformOs)
 	});
 }
--- firefox/content.js
+++ firefox/content.js
@@ -3,6 +3,11 @@ const PATH = document.location.pathname;
 //const BASE_URI = document.location.baseURI; //protocol://host/path
 const ORIGIN = document.location.origin;
 
+const DOMAIN_REGEX = new RegExp("^"+ORIGIN+".+", "g");
+const IMAGES_REGEX = /\.(gif|jpe?g|tiff|png|webp|bmp|ico|svg|ai|eps)$/i
+const VIDEOS_REGEX = /\.(mp4|m4p|m4v|mpg|webm|avi|wmv|mov|qt|flv|swf)$/i
+const AUDIO_REGEX = /\.(mp3|wav|wma|ogg|flac|midi?|m4a|pcast|mpa|aiff?)$/i
+const DOCUMENT_REGEX = /\.(docx?|xlsx?|pdf|mobi|epub|awz|txt|rtf|odt)$/i
 
 //go thru links and add to the context menu an option to make a wget list, or download all links on current domain, probably pop a menu
 
@@ -16,8 +21,30 @@ browser.runtime.onConnect.addListener(function(port){
 		console.log("msg recieved: "+JSON.stringify(msg));
 		//alert("msg recieved");
 		//only one type currently so make link list
+		let regex = DOMAIN_REGEX;
+		switch(msg.action){
+			case "domain":
+				regex = DOMAIN_REGEX;
+				break;
+			case "images":
+				regex = IMAGES_REGEX;
+				break;
+			case "videos":
+				regex = VIDEOS_REGEX;
+				break;
+			case "audio":
+				regex = AUDIO_REGEX;
+				break;
+			case "documents":
+				regex = DOCUMENT_REGEX;
+				break;
+			case "custom":
+			default:
+				//TODO
+				break;
+		}
 		port.postMessage(msg);
-		generateLinkList(msg.newline);
+		generateLinkList(regex, msg.newline);
 	});
 	console.log("connection recieved!");
 });
@@ -25,22 +52,23 @@ browser.runtime.onConnect.addListener(function(port){
 
 //newline param for win vs unix like systems
 //TODO let people manually set these if they prefer as wel as the autodetect
-function generateLinkList(newline){
+//TODO allow search of selected text/html instead of whole page
+function generateLinkList(pattern, newline){
 	let links = document.getElementsByTagName("A");
 	let list = "";
 	let hmap = {}; //hashmap to help remove duplicate links
 
-	let pattern = new RegExp("^"+ORIGIN+PATH+".+", "g"); //href contains but is not equal to the baseURI
+	//let pattern = new RegExp("^"+ORIGIN+PATH+".+", "g"); //href contains but is not equal to the baseURI
 		//NOTE the above only works for hierarchical websites, if thepaths do not follow a hierarchy, then this will fail
 	let patternAlt = new RegExp("^"+ORIGIN+".+", "g"); //works on non-hierarchical paths, may cause infinite loop. to prevent have bg.js keep a hash of all seen links for this page and do not revisit any.
-	console.log(patternAlt);
+	console.log(pattern);
 
 
 	for(let i=0; i < links.length; i++){
 		let link = links[i].href;
 		
-		console.log(link.match(patternAlt));
-		if(link.match(patternAlt)){
+		console.log(link.match(pattern));
+		if(link.match(pattern)){
 			hmap[link] = link;
 			//list += link+"\n"; //TODO make Windows compatible based on host OS, macos uses \n now i think
 		}
@@ -48,7 +76,7 @@ function generateLinkList(newline){
 	//now use hmap to remove dupes
 	let uniqueLinks = Object.keys(hmap);
 	for(let i=0; i < uniqueLinks.length; i++){
-		list += uniqueLinks[i]+newline; //TODO think of the windows users
+		list += uniqueLinks[i]+newline;
 	}
 
 	createTextDownload(list);
